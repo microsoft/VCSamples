92bcad05210499e017c66c4c62c927dcc680d4ee
Eric Mittelette <ericmitt@corp.microsoft.com>
2016-10-18 14:25:19 -0700

    hilo sample and ODBC sample for 2015

---
+++ VC2015Samples/Hilo/C++/Annotator/RibbonIds.h
@@ -0,0 +1,96 @@
+// *****************************************************************************
+// * This is an automatically generated header file for UI Element definition  *
+// * resource symbols and values. Please do not modify manually.               *
+// *****************************************************************************
+
+#pragma once
+
+#define IDC_CMD_HOMETAB 10000 
+#define IDC_CMD_HOMETAB_LabelTitle_RESID 200
+#define IDC_CMD_VIEWTAB 20000 
+#define IDC_CMD_VIEWTAB_LabelTitle_RESID 700
+#define linkGroup 2 
+#define linkGroup_LabelTitle_RESID 210
+#define zoomGroup 3 
+#define zoomGroup_LabelTitle_RESID 220
+#define editGroup 4 
+#define editGroup_LabelTitle_RESID 230
+#define pencilButton 5 
+#define pencilButton_LabelTitle_RESID 240
+#define pencilButton_SmallImages_RESID 242
+#define pencilButton_LargeImages_RESID 241
+#define sizeButton 6 
+#define sizeButton_LabelTitle_RESID 260
+#define sizeButton_SmallImages_RESID 262
+#define sizeButton_LargeImages_RESID 261
+#define cropButton 7 
+#define cropButton_LabelTitle_RESID 280
+#define cropButton_SmallImages_RESID 281
+#define cropButton_LargeImages_RESID 271
+#define rotateButton 8 
+#define rotateButton_LabelTitle_RESID 290
+#define rotateButton_SmallImages_RESID 292
+#define rotateButton_LargeImages_RESID 291
+#define rotate90Clockwise 9 
+#define rotate90Clockwise_LabelTitle_RESID 293
+#define rotate90Clockwise_SmallImages_RESID 295
+#define rotate90Clockwise_LargeImages_RESID 294
+#define rotate90CounterClockwise 10 
+#define rotate90CounterClockwise_LabelTitle_RESID 296
+#define rotate90CounterClockwise_SmallImages_RESID 298
+#define rotate90CounterClockwise_LargeImages_RESID 297
+#define ID_BUTTON_ZOOM_IN 300 
+#define ID_BUTTON_ZOOM_IN_LabelTitle_RESID 300
+#define ID_BUTTON_ZOOM_IN_SmallImages_RESID 302
+#define ID_BUTTON_ZOOM_IN_LargeImages_RESID 301
+#define ID_BUTTON_ZOOM_OUT 310 
+#define ID_BUTTON_ZOOM_OUT_LabelTitle_RESID 310
+#define ID_BUTTON_ZOOM_OUT_SmallImages_RESID 312
+#define ID_BUTTON_ZOOM_OUT_LargeImages_RESID 311
+#define ID_BUTTON_ZOOM_100 320 
+#define ID_BUTTON_ZOOM_100_LabelTitle_RESID 320
+#define ID_BUTTON_ZOOM_100_SmallImages_RESID 322
+#define ID_BUTTON_ZOOM_100_LargeImages_RESID 321
+#define ID_STANDARD_COLORPICKER 11 
+#define ID_STANDARD_COLORPICKER_LabelTitle_RESID 330
+#define ID_STANDARD_COLORPICKER_SmallImages_RESID 332
+#define ID_STANDARD_COLORPICKER_LargeImages_RESID 331
+#define flipVertical 12 
+#define flipVertical_LabelTitle_RESID 340
+#define flipVertical_SmallImages_RESID 342
+#define flipVertical_LargeImages_RESID 341
+#define flipHorizontal 13 
+#define flipHorizontal_LabelTitle_RESID 350
+#define flipHorizontal_SmallImages_RESID 352
+#define flipHorizontal_LargeImages_RESID 351
+#define cmdQat 14 
+#define ID_BUTTON_UNDO 15 
+#define ID_BUTTON_UNDO_LabelTitle_RESID 400
+#define ID_BUTTON_UNDO_SmallImages_RESID 402
+#define ID_BUTTON_UNDO_LargeImages_RESID 401
+#define ID_BUTTON_REDO 16 
+#define ID_BUTTON_REDO_LabelTitle_RESID 410
+#define ID_BUTTON_REDO_SmallImages_RESID 412
+#define ID_BUTTON_REDO_LargeImages_RESID 411
+#define ID_FILE_MENU 500 
+#define ID_FILE_OPEN 17  /* Open */ 
+#define ID_FILE_OPEN_LabelTitle_RESID 520
+#define ID_FILE_OPEN_SmallImages_RESID 522
+#define ID_FILE_OPEN_LargeImages_RESID 521
+#define ID_FILE_SAVE 530 
+#define ID_FILE_SAVE_LabelTitle_RESID 530
+#define ID_FILE_SAVE_Keytip_RESID 60001
+#define ID_FILE_SAVE_TooltipTitle_RESID 60002
+#define ID_FILE_SAVE_TooltipDescription_RESID 60003
+#define ID_FILE_SAVE_SmallImages_RESID 532
+#define ID_FILE_SAVE_LargeImages_RESID 531
+#define ID_FILE_SAVE_AS 550 
+#define ID_FILE_SAVE_AS_LabelTitle_RESID 550
+#define ID_FILE_SAVE_AS_TooltipTitle_RESID 60004
+#define ID_FILE_SAVE_AS_TooltipDescription_RESID 60005
+#define ID_FILE_SAVE_AS_SmallImages_RESID 552
+#define ID_FILE_SAVE_AS_LargeImages_RESID 551
+#define ID_FILE_EXIT 18  /* Exit */ 
+#define ID_FILE_EXIT_LabelTitle_RESID 560
+#define ID_FILE_EXIT_SmallImages_RESID 562
+#define ID_FILE_EXIT_LargeImages_RESID 561
